f25ad2e55fd92076b6771abf15856e7087fced14
FatimaAli1309 <158024222+FatimaAli1309@users.noreply.github.com>
2024-02-12 14:18:17 +0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
+import CartScreen from './src/screen/cartScreen';
+const App = () => {
+  return (
+  <NavigationContainer>
+        <CartScreen/>
+  </NavigationContainer>
+)
+}
+export default App;
